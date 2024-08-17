82095dc9a29898e9ed66a749ea1ff716c3c59883
CarolineRoemer1998 <caroroemer@gmx.de>
2024-08-17 16:35:08 +0200

    Highlight Objects when detected
    Co-Authored-By: T4NT1N <32192242+T4NT1N@users.noreply.github.com>

--- Player/ObjectDetector.gd
+++ Player/ObjectDetector.gd
@@ -2,6 +2,8 @@ extends Area3D
 
 @onready var collision_shape_3d: CollisionShape3D = $CollisionShape3D
 
+var current_detected = null
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,5 +14,16 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_body_entered(body: Node3D) -> void:
-		print(body.name)
+func _on_area_entered(area: Area3D) -> void:
+	print(area.name)
+	current_detected = area
+	for child in area.get_parent().get_children():
+		if child.name == "Highlight":
+			child.visible = true
+
+
+func _on_area_exited(area: Area3D) -> void:
+	current_detected = null
+	for child in area.get_parent().get_children():
+		if child.name == "Highlight" and child.visible == true:
+			child.visible = false
