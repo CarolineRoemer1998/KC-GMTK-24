2969b313f917759e1c8e87164709bc70a5c19450
CarolineRoemer1998 <caroroemer@gmx.de>
2024-08-20 16:04:46 +0200

    Update Button and Selling Plants

--- User Interface/Market/market_ui.gd
+++ User Interface/Market/market_ui.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 @onready var selling_plant = $Panel/Selling_Plant
+@onready var button = $Panel/Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,20 +9,35 @@ func _ready():
 
 var player: Player
 var plant: Plant
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 func show_market(entered_player : Player):
 	
 	player = entered_player
-	plant = player.interaction.carrying_plant
 	visible = true
-	if plant != null:
+	if player.interaction.carrying_plant != null:
+		plant = player.interaction.carrying_plant
 		selling_plant.fill_stats(plant)
+		selling_plant.set_plant_image(plant)
+		button.text = "Sell " + str(get_plant_type(plant.type)) + " for: " + str(selling_plant.get_plant_price(plant))
 
+func get_plant_type(type: Plant.plant_type):
+	match type:
+		Plant.plant_type.carrot:
+			return "carrot"
+		Plant.plant_type.cauliflower:
+			return "cauliflower"
+		Plant.plant_type.zucchini:
+			return "zucchini"	
+		Plant.plant_type.strawberry:
+			return "strawberry"
 func close_market():
 	visible = false
 
 func _on_button_pressed():
-	pass
+	if player.interaction.carrying_plant != null:
+		Global.gain_money(selling_plant.get_plant_price(plant))
+		
+		player.carrying_weight = Player.Weight.none
+		plant.queue_free()
+		close_market()
+		selling_plant.remove_plant()
