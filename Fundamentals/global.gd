8036a3de24fa1d663cfb4b7bb5618b03d9d3e45a
cora_lyn <caroroemer@gmx.de>
2024-09-07 21:29:54 +0200

    Implemented changing daylight

--- Fundamentals/global.gd
+++ Fundamentals/global.gd
@@ -7,6 +7,19 @@ var current_money: int
 var start_energy : int = 8
 var current_energy : int
 
+var sun : DirectionalLight3D
+var sun_directions = {
+	7: [-8, 98, Color(0.83,0.65,1.0)],
+	8: [-24, 70, Color(1.0,0.6,0.8)],
+	6: [-40, 42, Color(1.0, 0.6, 0.6)],
+	5: [-56, 14, Color(1.0, 0.8, 0.7)],
+	4: [-56, -14, Color(1.0, 0.65, 0.35)],
+	3: [-40, -42, Color(1.0, 0.37, 0.25)],
+	2: [-24, -70, Color(0.8, 0.1, 0.1)],
+	1: [-8, -98, Color(0.1, 0.0, 0.9)],
+	0: [-8, -98, Color(0.1, 0.0, 0.9)]
+}
+
 var energy_bar 
 var game_handler
 var player: Player
@@ -23,22 +36,29 @@ enum MINIGAME_TYPE {watering, lawnmowing}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	energy_bar = get_tree().get_first_node_in_group("EnergyBar")
 	game_handler = get_tree().get_first_node_in_group("GameHandler")
 	player = get_tree().get_first_node_in_group("Player")
+	sun = get_tree().get_first_node_in_group("Sun")
+	rotate_sun()
 	
 	
 	current_energy = start_energy
 	current_money = start_money
 
 func lose_energy(energy_cost: int):
+	printerr(current_energy)
 	current_energy -= energy_cost
 	energy_bar.update_slices()
-	print(current_energy)
 	if current_energy <= 0:
 		game_handler.end_day()
+	rotate_sun()
+		
+
+func rotate_sun():
+	sun.rotation_degrees = Vector3(sun_directions[current_energy][0], sun_directions[current_energy][1], 0)
+	sun.light_color = sun_directions[current_energy][2]
 
 func refill_energy():
 	if current_energy == 0:
@@ -46,6 +66,7 @@ func refill_energy():
 	else:
 		current_energy = Global.start_energy 
 	energy_bar.update_slices()
+	rotate_sun()
 	
 func spend_money(amount : int):
 	current_money = clamp(current_money - amount,0,9999)
